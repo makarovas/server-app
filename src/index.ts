08cc42a2431babd6024c1b3b968a03b69dbed1df
Alexander Makarof <makarofalex@gmail.com>
2020-03-02 20:57:00 +0300

    add graphql shcema

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,17 @@
 import express from "express";
-import { listings } from './listings';
-import bodyParser from 'body-parser';
+import {
+  ApolloServer
+} from 'apollo-server-express';
+import {
+  schema
+} from './graphql';
+
 
 const app = express();
 const port = 9000;
-
-
-app.use(bodyParser.json());
-
-
-//// routes
-// get
-app.get("/", (_req, res, ) => res.send("ping pong"));
-app.get('/listings', (_req, res) => {
-  return res.send(listings);
+const server = new ApolloServer({ schema });
+server.applyMiddleware({
+  app,
+  path: '/api'
 });
-
-//post
-app.post('/delete-listing', (req, res) => {
-  const id: string = req.body.id;
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-  return res.send('failed to deleted listings');
-});
-
 app.listen(port);
-
-
