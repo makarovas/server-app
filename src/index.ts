50cf4e328663dc7f977b6789089b867a11b232ae
Alexander Makarof <makarofalex@gmail.com>
2020-03-02 11:34:30 +0300

    added base get/post routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
 import express from "express";
+import { listings } from './listings';
+import bodyParser from 'body-parser';
+
 const app = express();
 const port = 9000;
 
-app.get("/", (_req, res) => res.send("ping pong pone"));
+
+app.use(bodyParser.json());
+
+
+//// routes
+// get
+app.get("/", (_req, res, ) => res.send("ping pong"));
+app.get('/listings', (_req, res) => {
+  return res.send(listings);
+});
+
+//post
+app.post('/delete-listing', (req, res) => {
+  const id: string = req.body.id;
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+  return res.send('failed to deleted listings');
+});
+
 app.listen(port);
+
+
